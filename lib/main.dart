fcf6ed1366df90232452b6d440b3bbb460bf7583
Rayhan Ahmed Imran <114678962+ai-rayhan@users.noreply.github.com>
2023-09-30 22:03:37 +0600

    module 8 assainment

--- lib/main.dart
+++ lib/main.dart
@@ -7,122 +7,104 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-       primarySwatch: Colors.blue
+    return const MaterialApp(
+      home: Scaffold(
+        body: Homepage(),
       ),
-      home: const MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
-  static const List<String> imageLink = [
-    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRjGEIKf5RT-lxMxx_qOrK4opqQMOecvvGn9w&usqp=CAU',
-    'https://upload.wikimedia.org/wikipedia/commons/thumb/4/48/RedCat_8727.jpg/1200px-RedCat_8727.jpg',
-    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSAtxqSrBSeFoj-zXpQdUjxqhLJU86mkhndwg&usqp=CAU',
-    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcThddPBu4Y7Z_msmV_fE_HXLS5QG4IXJaj5aw&usqp=CAU',
-    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR5ZNus26cImi0EkSyzjxSIKjgILsc8lbExjg&usqp=CAU',
-    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRU4cXYbxLC5LU7i-9tjPQBxSAszrI1ZJCctg&usqp=CAU'
-  ];
+class Homepage extends StatelessWidget {
+  const Homepage({super.key});
+
   @override
   Widget build(BuildContext context) {
+    final orientation = MediaQuery.of(context).orientation;
     return Scaffold(
         appBar: AppBar(
-          title: const Text('Photo Gallery'),
-          centerTitle: true,
+          title: const Text("Profile"),
         ),
         body: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              const Padding(
-                padding: EdgeInsets.all(16.0),
-                child: Text(
-                  'Welcome to My Photo Gallery!',
-                  style: TextStyle(fontSize: 24.0),
-                ),
-              ),
-              const Padding(
-                padding: EdgeInsets.all(16.0),
-                child: TextField(
-                  decoration: InputDecoration(
-                    hintText: 'Search for photos',
-                    border: OutlineInputBorder(),
-                  ),
-                ),
-              ),
-              SizedBox(
-                child: Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: GridView.builder(
-                    physics: const NeverScrollableScrollPhysics(),
-                    shrinkWrap: true,
-                    itemCount: imageLink.length,
-                    gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 3,
-                      mainAxisSpacing: 30,
-                      crossAxisSpacing: 30,
-                    ),
-                    itemBuilder: (context, index) {
-                      return GridTile(
-                        footer: Container(color: Colors.white,child: Padding(
-                          padding: const EdgeInsets.only(top: 10),
-                          child: Center(child: Text("Photo $index",style: const TextStyle(fontSize: 20),)),
-                        ),),
-                        child: GestureDetector(
-                          onTap: () {
-                            ScaffoldMessenger.of(context).showSnackBar(
-                              SnackBar(
-                                content: Text('Image $index clicked!'),
-                              ),
-                            );
-                          },
-                          child: Container(
-                            height: 150,
-                            color: Colors.grey,
-                            child: Image.network(
-                              imageLink[index],
-                              fit: BoxFit.cover,
-                            ),
-                          ),
-                        ),
-                      );
-                    },
-                  ),
-                ),
-              ),
-              SizedBox(
-                child: ListView.builder(
-                  itemCount: 3,
-                  shrinkWrap: true,
-                   physics: const NeverScrollableScrollPhysics(),
-                  itemBuilder:  (context, iindex) =>  ListTile(
-                    leading: CircleAvatar(backgroundImage:NetworkImage(imageLink[iindex]),),
-                  title: Text('Photo ${iindex+1}'),
-                  subtitle: Text('Description of photo ${iindex+1}'),
-                ),
-                         ),
-              ),
-              const SizedBox(height: 10,),
-              FloatingActionButton(
-              onPressed: () {
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    const SnackBar(
-                      content: Text('Photos Uploaded Successfully!'),
-                    ),
-                  );
-                },
-                child: const Icon(Icons.upload),
-              ),
-            ],
+            child: orientation == Orientation.portrait
+                ? const Column(
+                    children: [ProfilePic(), PhotoGraphy()],
+                  )
+                : const Row(
+                    children: [ProfilePic(), PhotoGraphy()],
+                  )));
+  }
+}
+
+class PhotoGraphy extends StatelessWidget {
+  const PhotoGraphy({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final orientation = MediaQuery.of(context).orientation;
+    final size = MediaQuery.of(context).size;
+    return SizedBox(
+      width: orientation == Orientation.portrait
+          ? size.width / 1.2
+          : size.width / 1.7,
+      child: Column(
+        children: [
+          const Center(
+              child: Text(
+            "John Doe",
+            style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+          )),
+          const Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Text(
+              "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type.",
+            ),
           ),
+          SizedBox(
+            child: GridView.builder(
+              shrinkWrap: true,
+              physics: const NeverScrollableScrollPhysics(),
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 3,
+                mainAxisSpacing:
+                    10.0, // Adjust this value to reduce main axis spacing
+                crossAxisSpacing: 10.0,
+              ),
+              itemCount: 6,
+              itemBuilder: (context, index) {
+                return Image.network(
+                    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS9rpQDNSwZQAbXaZwxN6AgHWan5HBoj-9uKQ&usqp=CAU');
+              },
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class ProfilePic extends StatelessWidget {
+  const ProfilePic({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final orientation = MediaQuery.of(context).orientation;
+    final size = MediaQuery.of(context).size;
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Center(
+        child: CircleAvatar(
+          radius: orientation == Orientation.portrait ? 200 : size.width / 6,
+          backgroundImage: const NetworkImage(
+              "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwLU8Bv0Xrg8zWF8OmdAE31XhRQJlw9rJfWQ&usqp=CAU"),
         ),
-      );
+      ),
+    );
   }
 }
